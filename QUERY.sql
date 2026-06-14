3e8a568d79b9e82fd6055a03ff06670727199b7f
Mohaiminul375 <mohaiminul375@gmail.com>
2026-06-14 09:19:36 +0600

    create all table

--- QUERY.sql
+++ QUERY.sql
@@ -5,8 +5,24 @@ CREATE TABLE Users (
     email varchar(255) unique not null,
     role varchar(20) check (role in ('Ticket Manager', 'Football Fan')),
     phone_number varchar(14)
-    -- Write your constraint to make 'user_id' the Primary Key
-    -- Write your constraint to ensure 'email' values are never duplicated
-    -- Write your check constraint to restrict 'role' to specific allowed strings
-
 );
+
+-- 2. CREATE MATCHES TABLE
+CREATE TABLE Matches (
+    match_id serial primary key,
+    fixture varchar(255) not null,
+    tournament_category varchar(255) not null,
+    base_ticket_price int check(base_ticket_price >= 0) not null,
+    match_status varchar(20) check ( match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed')) not null
+);
+
+-- 3. CREATE BOOKINGS TABLE
+CREATE TABLE Bookings (
+    booking_id serial primary key,
+    user_id smallint references users(user_id) not null,
+    match_id smallint references matches(match_id) not null,
+    seat_number varchar(10),
+    payment_status varchar(10) check(payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost int check(total_cost >= 0) not null
+);
+
